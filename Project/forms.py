7f2e75651107c56cdb71d8811dd3be44a098866c
nickker3 <nickbakker@gmail.com>
2023-01-30 19:35:24 +0100

    bugfix

--- Project/forms.py
+++ Project/forms.py
@@ -13,12 +13,12 @@ class AddFilmForm(FlaskForm):
 
 class AddRegisseurForm(FlaskForm):
     voornaam = StringField('Voornaam van de Regisseur:')
-    achternaam = StringField('Achternaam van de Regiseur')
+    achternaam = IntegerField('Achternaam van de Regiseur')
     submit = SubmitField('Voeg de Regiseur toe')
 
 class AddActeurForm(FlaskForm):
     voornaam = StringField('Voornaam van de Acteur:')
-    achternaam = StringField('Achternaam van de Acteur')
+    achternaam = IntegerField('Achternaam van de Acteur')
     submit = SubmitField('Voeg de Acteur toe')
 
 class DelFilmForm(FlaskForm):
@@ -26,27 +26,18 @@ class DelFilmForm(FlaskForm):
     id = IntegerField('Welke Film wil je verwijderen? (ID):')
     submit = SubmitField('Verwijder Film')
 
-class DelRegisseurForm(FlaskForm):
-
-    id = IntegerField('Welke regisseur wil je verwijderen? (ID):')
-    submit = SubmitField('Verwijder regisseur')
-
-class DelActeurForm(FlaskForm):
-
-    id = IntegerField('Welke acteur wil je verwijderen? (ID):')
-    submit = SubmitField('Verwijder acteur')
 
 class LoginForm(FlaskForm):
 #    email = StringField('Email',validators=[DataRequired(),Email()])
     Password = PasswordField('Password', validators=[DataRequired()])
-    Submit = SubmitField("Log in")
+    Submit = SubmitField("log in")
 
 class RegistrationForm(FlaskForm):
 #    email = StringField('Email',validators=[DataRequired(),Email()])
     username = StringField('Gebruikersnaam', validators=[DataRequired()])
-    passwoord = PasswordField('Wachtwoord', validators=[DataRequired(),EqualTo('pass_confirm',message='Wachtwoorden komenn niet overeen')])
+    passwoord = PasswordField('Wachtwoord', validators=[DataRequired(),EqualTo('pass_confirm',message='Wachtwoorden komenn nniet over een')])
     pass_confirm = PasswordField('herhaal Wachtwoord',validators=[DataRequired()])
-    submit = SubmitField('Registreer!')
+    submit = SubmitField('registreer!')
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
