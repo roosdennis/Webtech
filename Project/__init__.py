f8c5a3f2454031c28fdc3e423c77bd5b74f55695
nickker3 <nickbakker@gmail.com>
2023-01-30 15:37:32 +0100

    changed __init__.py

--- Project/__init__.py
+++ Project/__init__.py
@@ -1,1 +1,17 @@
 #__init__.py
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///'+os.path(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
