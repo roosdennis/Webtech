f4f9c99ce64e8199e570fcb1d283f013a316baa3
Dennis Roos <dennisroos87@gmail.com>
2023-01-30 17:39:14 +0100

    changed import rules

--- Project/models.py
+++ Project/models.py
@@ -1,5 +1,5 @@
 import os
-from forms import  AddFilmForm , DelFilmForm, AddRegisseurForm, AddActeurForm, DelRegisseurForm, DelActeurForm
+from forms import  AddFilmForm , DelFilmForm, AddRegisseurForm, DelRegisseurForm, AddActeurForm, DelActeurForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
