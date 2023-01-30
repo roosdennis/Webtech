eb99bd0fb1d5c31b00185e57262cb30add1beea4
nickker3 <nickbakker@gmail.com>
2023-01-30 17:06:55 +0100

    changed forms.py

--- Project/forms.py
+++ Project/forms.py
@@ -1,5 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+
+
 
 class AddFilmForm(FlaskForm):
     titel = StringField('Titel van de Film:')
@@ -21,3 +25,24 @@ class DelFilmForm(FlaskForm):
 
     id = IntegerField('Welke Film wil je verwijderen? (ID):')
     submit = SubmitField('Verwijder Film')
+
+
+class LoginForm(FlaskForm):
+#    email = StringField('Email',validators=[DataRequired(),Email()])
+    Password = PasswordField('Password', validators=[DataRequired()])
+    Submit = SubmitField("log in")
+
+class RegistrationForm(FlaskForm):
+#    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = StringField('Gebruikersnaam', validators=[DataRequired()])
+    passwoord = PasswordField('Wachtwoord', validators=[DataRequired(),EqualTo('pass_confirm',message='Wachtwoorden komenn nniet over een')])
+    pass_confirm = PasswordField('herhaal Wachtwoord',validators=[DataRequired()])
+    submit = SubmitField('registreer!')
+
+    def check_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Dit email adress is al in gebruik!')
+
+    def check_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('deze Gebruikersnaam is al in gebruik.')
